da8a865b5ccf95801eafc26b54c9f1efb35430bd
Jhon Roly Projects <92251336+jhonrolyol@users.noreply.github.com>
2022-10-20 18:08:34 -0500

    Commit

---
+++ 02_VariablesTiposDeDatos/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Preamble:
+        ' Note1.- Usar "F5" para compilar.
+        ' Note2.- Usar "Dim" para crear variables.
+
+        ' Main:
+        ' Main1.- Crear la variable "entero" usando "Integer".
+        Dim entero As Integer = 1
+        MsgBox(entero) ' Muestra el resultado en una ventana aparte.
+        entero = 2
+        MsgBox(entero) ' Muestra el resultado en una ventana aparte.
+
+        ' Main2.- Crear la variable "numero" usando "Double".
+        Dim numero As Double = 123.456
+        MsgBox(numero) ' Muestra el resultado en una ventana aparte.
+        numero = 12.5
+        MsgBox(numero) ' Muestra el resultado en una ventana aparte.
+
+        ' Main3.- Crear la variable "texto" usando "String".
+        Dim texto As String = "Hola Mundo"
+        MsgBox(texto) ' Muestra el resultado en una ventana aparte.
+        Dim texto2 As String = "Código Facilito"
+        MsgBox(texto + texto2) ' Muestra el resultado en una ventana aparte.
+
+        ' Main4.- Crear la variable "fecha" usando "Date".
+        'Dim fecha As Date = "01/01/12"
+        Dim fecha As Date = "01-01-12" ' Día/Mes/Año
+        MsgBox(fecha) ' Muestra el resultado en una ventana aparte.
+        Dim fecha2 As Date = "01-JAN-2022"
+        MsgBox(fecha2) ' Muestra el resultado en una ventana aparte.
+
+
+    End Sub
+End Class
---
+++ 03_Arrays/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Preamble:
+        ' Note1.- Usar "F5" para compilar.
+        ' Note2.- Usar "Control + S" para guardar el archivo.
+
+        ' Main:
+        ' Main1.- Crear un arreglo con 3 posiciones
+        Dim arreglo(3) As String
+        arreglo(0) = "Hola" 'Posición 1
+        arreglo(1) = "Código" ' Posición 2
+        arreglo(2) = "Facilito" ' Posición 3
+        MsgBox(arreglo(0) + " " + arreglo(1) + " " + arreglo(3)) ' Usar " " para espaciado.
+
+        ' Main2.- Crear un arreglo con ...... posiciones
+        Dim arreglo() As Integer
+        arreglo = New Integer() {1, 2, 3}
+        MsgBox(arreglo(0).ToString + " " + arreglo(1).ToString + " " + arreglo(2).ToString)
+
+        ReDim Preserve arreglo(2) ' Usar "Preserve" para mantener los datos de arreglo.
+        MsgBox(arreglo(0).ToString + " " + arreglo(1).ToString)
+
+        ReDim arreglo(3)
+        MsgBox(arreglo(0).ToString + " " + arreglo(1).ToString + " " + arreglo(2).ToString)
+
+        ' Main3.- Matriz
+        Dim matriz(,) As Integer = {{1, 2, 3}, {4, 5, 6}}
+        MsgBox(matriz(1, 1).ToString)
+
+
+    End Sub
+End Class
